d3f27eb550e5645d7e78c58c71c2ba3cb10c5f73
230101 <230101@LTI1402>
2024-08-27 16:37:06 -0300

    Adicionar arquivos de projeto.

---
+++ Arduino/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Arduino
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.txtReceber = new System.Windows.Forms.TextBox();
+            this.txtEnviar = new System.Windows.Forms.TextBox();
+            this.cmbPortas = new System.Windows.Forms.ComboBox();
+            this.tmrPortas = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(12, 12);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(100, 28);
+            this.btnConectar.TabIndex = 0;
+            this.btnConectar.Text = "&Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(12, 57);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(100, 26);
+            this.btnEnviar.TabIndex = 1;
+            this.btnEnviar.Text = "&Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            // 
+            // txtReceber
+            // 
+            this.txtReceber.Location = new System.Drawing.Point(12, 100);
+            this.txtReceber.Multiline = true;
+            this.txtReceber.Name = "txtReceber";
+            this.txtReceber.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtReceber.Size = new System.Drawing.Size(765, 338);
+            this.txtReceber.TabIndex = 3;
+            // 
+            // txtEnviar
+            // 
+            this.txtEnviar.Location = new System.Drawing.Point(132, 57);
+            this.txtEnviar.Name = "txtEnviar";
+            this.txtEnviar.Size = new System.Drawing.Size(558, 26);
+            this.txtEnviar.TabIndex = 4;
+            // 
+            // cmbPortas
+            // 
+            this.cmbPortas.FormattingEnabled = true;
+            this.cmbPortas.Location = new System.Drawing.Point(132, 12);
+            this.cmbPortas.Name = "cmbPortas";
+            this.cmbPortas.Size = new System.Drawing.Size(215, 28);
+            this.cmbPortas.TabIndex = 5;
+            // 
+            // tmrPortas
+            // 
+            this.tmrPortas.Interval = 1000;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbPortas);
+            this.Controls.Add(this.txtEnviar);
+            this.Controls.Add(this.txtReceber);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.btnConectar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox txtReceber;
+        private System.Windows.Forms.TextBox txtEnviar;
+        private System.Windows.Forms.ComboBox cmbPortas;
+        private System.Windows.Forms.Timer tmrPortas;
+    }
+}
+
---
+++ Arduino/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arduino
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort srpArduino;
+        public Form1()
+        {
+            InitializeComponent();
+            srpArduino = new SerialPort();
+            srpArduino.DataReceived += SrpArduino_DataReceived;
+        }
+
+        private void SrpArduino_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+           // txtReceber.Text ==
+          throw new NotImplementedException();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tmrPortas.Tick += tmrPortas_Tick;     
+            tmrPortas.Enabled = true;
+        }
+
+        private void tmrPortas_Tick(object sender, EventArgs e)
+        {
+           var i = 0;
+           var qtdDiferente = false;
+
+            if(cmbPortas.Items.Count == SerialPort.GetPortNames().Length) {
+
+                foreach (var porta in SerialPort.GetPortNames())
+                {
+                    if (!cmbPortas.Items[i++].Equals(porta))
+                    {
+                        qtdDiferente |= true;   
+                        break;
+                        
+                    }
+                }
+            
+            }
+            else
+            
+                qtdDiferente = true;
+            if (!qtdDiferente)
+                return;
+
+            cmbPortas.Items.Clear();
+
+            foreach (var porta in SerialPort.GetPortNames())
+            {
+                cmbPortas.Items.Add(porta);
+            }
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            if (!srpArduino.IsOpen)
+            {
+                try
+                {
+                    srpArduino.PortName = cmbPortas.SelectedText;
+                    srpArduino.Open();
+                    btnConectar.Text = "Desconectar";
+                    cmbPortas.Enabled = false;
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else
+
+                try
+                {
+                    srpArduino.Close();
+                    btnConectar.Text = "Conectar";
+                    cmbPortas.Enabled = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+        }
+    }
+}
